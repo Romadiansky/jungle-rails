ffd6fb7930f0c665ca0181d29c3df68c7010f67a
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-11-13 20:28:00 +0000

    created specs for validating the authenticate with credentials method; created the user.authenticate_with_credentials method; added support for spaces and upcase characters in emails; added a beautiful jumbotron image for your viewing pleasure

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,15 +23,17 @@
 
 class User < ActiveRecord::Base
 
-  validates :password, confirmation: { case_sensitive: true }
+  validates :password, confirmation: true
   validates :email, uniqueness:  { case_sensitive: false }
-  validates :email, :first_name, :last_name, presence: true
+  validates :email, :first_name, :last_name, :password_confirmation, presence: true
   validates :password, length: { in: 3..20 }
 
   has_secure_password
   has_many :reviews
 
   def self.authenticate_with_credentials(email, password)
+     user = User.find_by_email(email.delete(" \t\r\n").downcase)
+     user && user.authenticate(password)
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it 'should save successfully if all fields are properly filled out' do
+      @user = User.new
+      @user.first_name = 'first name'
+      @user.last_name = 'last name'
+      @user.email = 'user email'
+      @user.password = 'user password'
+      @user.password_confirmation = @user.password
+      expect(@user.save).to be true
+    end
+
+  it 'should have a first name' do
+      @user = User.new
+      @user.last_name = 'last name'
+      @user.email = 'user email'
+      @user.password = 'user password'
+      @user.password_confirmation = @user.password
+      expect(@user.save).to be false
+    end
+
+    it 'should have a last name' do
+      @user = User.new
+      @user.first_name = 'first name'
+      @user.email = 'user email'
+      @user.password = 'user password'
+      @user.password_confirmation = @user.password
+      expect(@user.save).to be false
+    end
+
+    it 'should have an email' do
+      @user = User.new
+      @user.first_name = 'first name'
+      @user.last_name = 'last name'
+      @user.password = 'user password'
+      @user.password_confirmation = @user.password
+      expect(@user.save).to be false
+    end
+
+    it 'should have a password' do
+      @user = User.new
+      @user.first_name = 'first name'
+      @user.last_name = 'last name'
+      @user.email = 'user email'
+      @user.password_confirmation = @user.password
+      expect(@user.save).to be false
+    end
+
+    it 'should have a password confirmation' do
+      @user = User.new
+      @user.first_name = 'first name'
+      @user.last_name = 'last name'
+      @user.email = 'user email'
+      @user.password = 'user password'
+      expect(@user.save).to be false
+    end
+
+    it 'should have a password confirmation equal to user password' do
+      @user = User.new
+      @user.first_name = 'first name'
+      @user.last_name = 'last name'
+      @user.email = 'user email'
+      @user.password = 'user password'
+      @user.password_confirmation = 'random other string'
+      expect(@user.save).to be false
+    end
+  end
+
+  before :each do
+    @user1 = User.create(email: 'tests@test.com', password: 'poi', password_confirmation: 'poi', first_name: 'name', last_name: 'madamname')
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'should authenticate successfully' do
+      login = User.authenticate_with_credentials('tests@test.com', 'poi')
+      expect(login).to eq(@user1)
+    end
+
+    it 'should fail to authenticate' do
+      login = User.authenticate_with_credentials('tests@test.com', 'xxx')
+      expect(login).to be false
+    end
+
+    it 'should allow people to login with spaces in their email addresses' do
+      login = User.authenticate_with_credentials(' tests@test.com ', 'poi')
+      expect(login).to eq(@user1)
+    end
+
+    it 'should allow logins with capital letters in the email address' do
+      login = User.authenticate_with_credentials('TeSts@test.com', 'poi')
+      expect(login).to eq(@user1)
+    end
+
+  end
+
 end
